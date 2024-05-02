40b4cd461e112c58e3d55aac6f85761af39d6635
Akash Papnai <akashpapnai@gmail.com>
2024-05-02 23:13:18 +0530

    changes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,4 +9,9 @@ import { NavbarComponent } from '../shared/navbar/navbar.component';
 })
 export class HomePageComponent {
   constructor() {}
+
+  items = [{src:"https://imgs.search.brave.com/fqTbQXrCdaCuYsdZGKwt8dMW5Vso_m2j671rQ-yMx8s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9waWNz/dW0ucGhvdG9zLzY0/MC80ODAvP2ltYWdl/PTM1Mw", alt: 'Image 1'},
+           {src:"https://imgs.search.brave.com/8EA6pnJcoQZoL-dyQtFMpmgZ-2B8nrntMEPZ88A72WM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2xhcmdlLXByZXZp/ZXdzL2Y0OC9yYW5k/b20tcGljcy0xLTEz/MjQyODcuanBnP2Zt/dA", alt: 'Image 2'},
+           {src:"https://imgs.search.brave.com/fqTbQXrCdaCuYsdZGKwt8dMW5Vso_m2j671rQ-yMx8s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9waWNz/dW0ucGhvdG9zLzY0/MC80ODAvP2ltYWdl/PTM1Mw", alt: 'Image 3'},
+  ]
 }
