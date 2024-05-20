ea6cd896acd32024d36c43572f2c7182e14b0ef3
Akash Papnai <akashpapnai@gmail.com>
2024-05-20 12:17:00 +0530

    docs changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HYRRXKIB.mjs";import"./chunk-ANJXTMUF.mjs";import"./chunk-CEBSNGSP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-FDUJ4GCE.mjs";import"./chunk-ANJXTMUF.mjs";import"./chunk-CEBSNGSP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
